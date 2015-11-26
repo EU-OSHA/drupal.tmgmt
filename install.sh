81b5b0e9f4706a2c30387bdf4fc7ed8307983ea4
Cristian Romanescu <cristian@romanescu.ro>
2015-11-26 19:08:58 +0200

    Enable demo during installation for the Vagrant box

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ -f sites/default/settings.php ]; then
     drush sql-drop -y
 fi
 drush site-install standard -y
-drush en -y osha_tmgmt
+drush en -y osha_tmgmt osha_tmgmt_demo
 drush en -y devel simpletest
 drush dis -y overlay
 drush cc all
